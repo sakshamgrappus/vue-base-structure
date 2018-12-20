29fa9950751c932d34c5d7a4bda657d000bdcd3e
Saksham BM Verenkar <saksham@grappus.com>
2018-12-20 10:39:41 +0530

    Project Initial Setup

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    autoprefixer({
+      add: true,
+      grid: true,
+    }),
+
+  ],
+};
